7da0597a08803f90ab1d9be2f1bfabdef67cd695
Danliang <i87960059@hotmail.com>
2020-03-22 14:29:03 -0400

    Add cropping image feature

--- health.py
+++ health.py
@@ -4,21 +4,25 @@ from pyspark.sql.types import *
 from pyspark.sql import SQLContext
 import os
 import pathlib
+from PIL import Image
+import pandas as pd
+import pyspark
+
+from resizeimage import resizeimage
+
+# def mod(x):
+#     import numpy as np
+#     return (x, np.mod(x, 2))
+# rdd = sc.parallelize(range(1000)).map(mod).take(10)
+# print(rdd)
 
 conf = SparkConf()
 conf.setMaster('local')
 conf.setAppName('test')
 sc = SparkContext(conf=conf)
+log4jLogger = sc._jvm.org.apache.log4j
+LOGGER = log4jLogger.LogManager.getLogger(__name__)
 sqlContext = SQLContext(sc)
-
-def mod(x):
-    import numpy as np
-    return (x, np.mod(x, 2))
-rdd = sc.parallelize(range(1000)).map(mod).take(10)
-print(rdd)
-
-
-patient_directories = [(f.path, f.name) for f in os.scandir("./CheXpert-v1.0-small/train") if f.is_dir()]
 
 
 def handle_patient_dir(patient_dir):
@@ -31,7 +35,33 @@ def handle_patient_dir(patient_dir):
             return ",".join([patient_name, patient_path, study_name, study_path, image_name, image_path, temp_path])
 
 
+def crop_all_images(df):
+    rdd = df.rdd.map(tuple)
 
-patient_directories_rdd = sc.parallelize(patient_directories)
-csv_rows = patient_directories_rdd.map( handle_patient_dir )
-csv_rows.saveAsTextFile("./frontal_images.csv")
+    def crop_image(row):
+        old_path = row[5]
+        new_path = row[6]
+        if old_path is None:
+            print("None old path")
+            return
+        with open(old_path, 'r+b') as f:
+            with Image.open(f) as image:
+                cover = resizeimage.resize_cover(image, [320, 320])
+                cover.save(new_path, image.format)
+
+    rdd.foreach(crop_image)
+
+
+def main():
+
+    patient_directories = [(f.path, f.name) for f in os.scandir("./CheXpert-v1.0-small/train") if f.is_dir()]
+    patient_directories_rdd = sc.parallelize(patient_directories)
+    csv_rows = patient_directories_rdd.map(handle_patient_dir)
+    csv_rows.saveAsTextFile("./frontal_images.csv")
+    image_crop_df = sqlContext.read.format('com.databricks.spark.csv').options(header='false', inferschema='true')\
+        .load('./frontal_images.csv/part-00000')
+    crop_all_images(image_crop_df)
+
+
+if __name__== "__main__":
+    main()
